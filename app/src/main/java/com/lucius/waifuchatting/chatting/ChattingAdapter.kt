f474c53c6f6e91de912a0e31290aa2cac947b87b
hongfei_ren <hongfei_ren@intsig.net>
2020-12-02 14:58:57 +0800

    [WN] [1.1.0] 首页Chatting内容-样式修改

--- app/src/main/java/com/lucius/waifuchatting/chatting/ChattingAdapter.kt
+++ app/src/main/java/com/lucius/waifuchatting/chatting/ChattingAdapter.kt
@@ -1,7 +1,7 @@
 package com.lucius.waifuchatting.chatting
 
+import android.widget.TextView
 import com.chad.library.adapter.base.BaseMultiItemQuickAdapter
-import com.chad.library.adapter.base.entity.MultiItemEntity
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.lucius.waifuchatting.R
 
@@ -13,5 +13,19 @@ class ChattingAdapter() : BaseMultiItemQuickAdapter<ChattingConversationEntity, 
 
     override fun convert(holder: BaseViewHolder, item: ChattingConversationEntity) {
         // todo 修改内容
+        // 1. 修改 个人/群 信息相关（头像、姓名、所属地）
+        holder.getView<TextView>(R.id.tv_name).text = "镜华"
+//        MyApplication.instance?.let {
+//            holder.getView<ImageView>(R.id.iv_avatar).setImageDrawable(
+//                ContextCompat.getDrawable(it, R.drawable.brvah_sample_footer_loading)
+//            )
+//        }
+        holder.getView<TextView>(R.id.tv_organization_and_title).text = "PCR | xcw"
+
+
+        // 2. 修改对话相关 基本信息 （未读数、最近消息、最新消息时间）
+        holder.getView<TextView>(R.id.tv_msg_count).text = "99+"
+        holder.getView<TextView>(R.id.tv_snd_content).text = "如果你这样方便的话，那我们就之后那里见吧~"
+        holder.getView<TextView>(R.id.tv_time).text = "6分钟前"
     }
 }
--- app/src/main/java/com/lucius/waifuchatting/chatting/ChattingFragment.kt
+++ app/src/main/java/com/lucius/waifuchatting/chatting/ChattingFragment.kt
@@ -1,12 +1,18 @@
 package com.lucius.waifuchatting.chatting
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 import com.lucius.comm.base.BaseFragment
+import com.lucius.waifuchatting.MyApplication
 import com.lucius.waifuchatting.R
 
 /**
@@ -22,6 +28,38 @@ class ChattingFragment : BaseFragment() {
         recyclerView = view?.findViewById<RecyclerView?>(R.id.recycler_view)
         recyclerView?.adapter = adapter
         recyclerView?.layoutManager = LinearLayoutManager(activity)
+        val decoration = object : ItemDecoration() {
+            private var marginLeft = 0
+            private var marginRight = 0
+            private var mDecorationHeight = 0
+            private var mPaint: Paint? = null
+
+            init {
+                this.marginLeft = marginLeft
+                this.marginRight = marginRight
+                mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+                mPaint?.color = Color.parseColor("#E9E9E9")
+                mPaint?.style = Paint.Style.FILL
+                mDecorationHeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 0.5f, MyApplication.instance?.resources?.displayMetrics).toInt()
+            }
+
+
+            override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+                super.onDrawOver(c, parent, state)
+                val childCount = parent.childCount
+                for (i in 0 until childCount - 1) {
+                    val child = parent.getChildAt(i)
+                    val left: Int = child.left + marginLeft
+                    val right: Int = child.right - marginRight
+                    val top = child.bottom
+                    val bottom: Int = top + mDecorationHeight
+                    mPaint?.let { c.drawRect(left.toFloat(), top.toFloat(), right.toFloat(), bottom.toFloat(), it) }
+                }
+            }
+        }
+
+        recyclerView?.addItemDecoration(decoration)
+
         adapter.addData(ChattingConversationEntity(TYPE_ENTITY_CHATTING_RAW_MESSAGE_SIMPLE))
         adapter.addData(ChattingConversationEntity(TYPE_ENTITY_CHATTING_RAW_MESSAGE_SIMPLE))
         adapter.addData(ChattingConversationEntity(TYPE_ENTITY_CHATTING_RAW_MESSAGE_SIMPLE))
